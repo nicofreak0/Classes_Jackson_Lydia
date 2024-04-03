62032da16d2a68274eafcbde8fd28a6f55326408
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-04-03 13:12:55 -0500

    greeting

---
+++ Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Jackson_Lydia
+{
+    internal class Class2
+    {
+    }
+}
---
+++ Greetings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Jackson_Lydia
+{
+    internal class Greetings
+    {
+
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome Traveler!");
+        }
+
+    }
+}
